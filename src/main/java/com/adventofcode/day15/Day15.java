c977a5afd48f872492bb3aaa56f5c8e7e216e706
Samo <samo.metlicar@hotmail.com>
2023-12-18 00:52:25 +0100

    day 15 - java util linked list

--- src/main/java/com/adventofcode/day15/Day15.java
+++ src/main/java/com/adventofcode/day15/Day15.java
@@ -5,6 +5,7 @@ import com.adventofcode.utils.Utils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.stream.IntStream;
 
 public class Day15 {
@@ -25,6 +26,42 @@ public class Day15 {
     }
 
     private static void part02(String[] input) {
+        var boxes = new ArrayList<LinkedList<Lens>>();
+        IntStream.range(0, 256).forEach(i -> boxes.add(new LinkedList<>()));
+        for (var instruction : input) {
+            var iDash = instruction.indexOf('-');
+            if (iDash < 0) {
+                var split = instruction.split("=");
+                var label = split[0];
+                var length = Integer.parseInt(split[1]);
+                var hash = hash(label);
+                boxes.get(hash).stream()
+                    .filter(lens -> lens.getLabel().equals(label))
+                    .findFirst()
+                    .ifPresentOrElse(
+                        lens -> lens.setLength(length),
+                        () -> boxes.get(hash).addLast(new Lens(label, length))
+                    );
+            } else {
+                var label = instruction.substring(0, iDash);
+                var hash = hash(label);
+                boxes.get(hash).removeIf(lens -> lens.getLabel().equals(label));
+            }
+        }
+
+        var result = 0;
+        for (var boxNumber = 0; boxNumber < 256; boxNumber++) {
+            var box = boxes.get(boxNumber);
+            var slotNumber = 1;
+            for (var lens : box) {
+                result += (boxNumber + 1) * (slotNumber++) * lens.getLength();
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static void part02Custom(String[] input) {
         var boxes = new ArrayList<CustomLinkedList<Lens>>(256);
         IntStream.range(0, 256).forEach(i -> boxes.add(new CustomLinkedList<>()));
         for (var instruction : input) {
